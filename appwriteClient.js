d055eac7847725237b79c6a57da8ad09107c358d
Prince <prince.chidera.dike@gmail.com>
2026-05-08 19:31:18 +0100

    migrating database to Appwrite

---
+++ appwriteClient.js
@@ -0,0 +1,28 @@
+// /Users/mac/Desktop/pidimarketing/appwriteClient.js
+
+import { Client, Account, Databases, Storage, ID, Query } from 'https://cdn.jsdelivr.net/npm/appwrite@13.0.1/+esm';
+
+const client = new Client();
+
+const APPWRITE_CONFIG = {
+    ENDPOINT: 'https://nyc.cloud.appwrite.io/v1',
+    PROJECT_ID: '69fdc475002a4730d4ce',
+    DATABASE_ID: 'pidi_tasks',
+    COLLECTIONS: {
+        PROFILES: 'profiles',
+        CAMPAIGNS: 'campaigns',
+        TRANSACTIONS: 'transactions',
+        ENGAGEMENTS: 'engagements',
+        ADMIN_WHITELIST: 'admin_whitelist'
+    },
+    BUCKETS: {
+        PROOFS: 'proofs'
+    }
+};
+
+client.setEndpoint(APPWRITE_CONFIG.ENDPOINT).setProject(APPWRITE_CONFIG.PROJECT_ID);
+
+export const account = new Account(client);
+export const databases = new Databases(client);
+export const storage = new Storage(client);
+export { ID, Query, APPWRITE_CONFIG };
